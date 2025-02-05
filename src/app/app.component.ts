fbf1a76c4849d63f996feeec7f0916a1a0ee0455
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2025-02-05 22:48:12 +0000

    some imporvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BeachDetailComponent } from './beach-detail/beach-detail.component';
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BeachDetailComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BeachDetailComponent } from './beach-detail/beach-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: BeachDetailComponent}
+];
