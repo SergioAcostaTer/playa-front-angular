15f771c69eb59fc2ba01ff2c2e9e3380b32d57d9
Miguel Ángel <migue.rodrii08@gmail.com>
2025-02-05 17:52:59 +0000

    beachdetails started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { BeachDetailComponent } from './beach-detail/beach-detail.component';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, BeachDetailComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'playa';
