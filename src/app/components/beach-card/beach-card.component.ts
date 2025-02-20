b78ab3f5bc0e5aac33117a910e349fbe32674ea7
Miguel Ángel <migue.rodrii08@gmail.com>
2025-02-20 17:36:48 +0000

    refactor beach card component and home page layout

--- src/app/components/beach-card/beach-card.component.ts
+++ src/app/components/beach-card/beach-card.component.ts
@@ -3,14 +3,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-beach-card',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './beach-card.component.html',
-  styleUrls: ['./beach-card.component.css']
+  styleUrls: ['./beach-card.component.css'],
+  host: { 'class': 'card' }
 })
 export class BeachCardComponent {
-  @Input() imageUrl: string = '';
-  @Input() title: string = '';
-  @Input() rating: string = '';
-  @Input() distance: string = '';
-  @Input() recommended: boolean = false;
+  @Input() imageUrl: string = 'assets/default-beach.jpg';
+  @Input() title: string = 'Playa Desconocida';
+  @Input() rating: string = '0';
+  @Input() distance: string = 'N/A';
+  @Input() recommended?: boolean = false;
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,93 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BeachCardComponent } from '../../components/beach-card/beach-card.component';
 
 @Component({
   selector: 'Home',
-  imports: [BeachCardComponent],
+  standalone: true,
+  imports: [CommonModule, BeachCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  beaches = [
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Playa_del_Verodal%2C_El_Hierro%2C_Canarias%2C_Espa%C3%B1a.JPG/220px-Playa_del_Verodal%2C_El_Hierro%2C_Canarias%2C_Espa%C3%B1a.JPG", title: "Playa del Verodal", rating: "4.5", distance: "A 10 km", recommended: true },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Tacor%C3%B3n_foto_principal.jpg/220px-Tacor%C3%B3n_foto_principal.jpg", title: "Playa de Tacorón", rating: "4.2", distance: "A 15 km" },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/San_Sebastian_EM1B5794_%2832684958331%29.jpg/220px-San_Sebastian_EM1B5794_%2832684958331%29.jpg", title: "Playa de San Sebastián", rating: "4.7", distance: "A 20 km", recommended: true },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Playa_de_Santiago_%288524842683%29.jpg/220px-Playa_de_Santiago_%288524842683%29.jpg", title: "Playa de Santiago", rating: "4.3", distance: "A 25 km" },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/View_of_the_steep_coast_at_Playa_De_Vueltas_black_sand_beach_on_La_Gomera%2C_Spain_%2848293845792%29.jpg/220px-View_of_the_steep_coast_at_Playa_De_Vueltas_black_sand_beach_on_La_Gomera%2C_Spain_%2848293845792%29.jpg", title: "Playa de Vueltas", rating: "4.6", distance: "A 30 km", recommended: true },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Santa_Cruz_de_La_Palma_2017-10_01.jpg/220px-Santa_Cruz_de_La_Palma_2017-10_01.jpg", title: "Playa de Santa Cruz de La Palma", rating: "4.5", distance: "A 35 km" },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/La_Palma_-_Brena_Baja_-_Los_Cancajos_-_Punta_de_la_Arena_%2B_Playa_de_Los_Cancajos_06_ies.jpg/220px-La_Palma_-_Brena_Baja_-_Los_Cancajos_-_Punta_de_la_Arena_%2B_Playa_de_Los_Cancajos_06_ies.jpg", title: "Playa de Los Cancajos", rating: "4.8", distance: "A 40 km", recommended: true },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/El_Faro_11_ies.jpg/220px-El_Faro_11_ies.jpg", title: "Playa El Faro", rating: "4.2", distance: "A 45 km" },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Playa_Charco_Verde_near_Puerto_Naos_%28Canary_Islands_2015%2C_La_Plama%29_-_panoramio.jpg/220px-Playa_Charco_Verde_near_Puerto_Naos_%28Canary_Islands_2015%2C_La_Plama%29_-_panoramio.jpg", title: "Playa Charco Verde", rating: "4.9", distance: "A 50 km", recommended: true },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/La_Palma_-_Tazacorte_-_El_Puerto_-_Avenida_El_Emigrante_%2B_Playa_08_ies.jpg/220px-La_Palma_-_Tazacorte_-_El_Puerto_-_Avenida_El_Emigrante_%2B_Playa_08_ies.jpg", title: "Playa de Tazacorte", rating: "4.3", distance: "A 55 km" },
+    { imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Puntallana_-_Playa_de_Nogales_01_ies.jpg/220px-Puntallana_-_Playa_de_Nogales_01_ies.jpg", title: "Playa de Nogales", rating: "4.6", distance: "A 60 km", recommended: true },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/PuntaDeTeno.jpg/220px-PuntaDeTeno.jpg",
+        title: "Playa de Punta de Teno",
+        rating: "4.7",
+        distance: "A 65 km",
+        recommended: true
+      },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Playa_de_El_Socorro.JPG/220px-Playa_de_El_Socorro.JPG",
+        title: "Playa de El Socorro",
+        rating: "4.5",
+        distance: "A 70 km"
+      },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Playa_Los_Patos.jpg/220px-Playa_Los_Patos.jpg",
+        title: "Playa Los Patos",
+        rating: "4.6",
+        distance: "A 75 km",
+        recommended: true
+      },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Las_Teresitas2a.jpg/220px-Las_Teresitas2a.jpg",
+        title: "Playa de Las Teresitas",
+        rating: "4.8",
+        distance: "A 80 km",
+        recommended: true
+      },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Playa_de_Alm%C3%A1ciga3.jpg/220px-Playa_de_Alm%C3%A1ciga3.jpg",
+        title: "Playa de Almáciga",
+        rating: "4.4",
+        distance: "A 85 km"
+      },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Playa_de_La_Nea.JPG/220px-Playa_de_La_Nea.JPG",
+        title: "Playa de La Nea",
+        rating: "4.3",
+        distance: "A 90 km"
+      },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Playa_de_San_Juan_555.jpg/220px-Playa_de_San_Juan_555.jpg",
+        title: "Playa de San Juan",
+        rating: "4.7",
+        distance: "A 95 km",
+        recommended: true
+      },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Playa_de_las_Am%C3%A9ricas_-_Playa_de_Torvoscas_%26_Playa_de_Fa%C3%B1abe.jpg/220px-Playa_de_las_Am%C3%A9ricas_-_Playa_de_Torvoscas_%26_Playa_de_Fa%C3%B1abe.jpg",
+        title: "Playa de Las Américas",
+        rating: "4.6",
+        distance: "A 100 km",
+        recommended: true
+      },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Playa_de_las_Vistas%2C_Abona%2C_Tenerife%2C_Canary_Islands%2C_Spain_-_panoramio.jpg/220px-Playa_de_las_Vistas%2C_Abona%2C_Tenerife%2C_Canary_Islands%2C_Spain_-_panoramio.jpg",
+        title: "Playa de Las Vistas",
+        rating: "4.5",
+        distance: "A 105 km"
+      },
+      {
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Playa_de_Guigui_%28II%29_-_panoramio.jpg/220px-Playa_de_Guigui_%28II%29_-_panoramio.jpg",
+        title: "Playa de Güigüí",
+        rating: "4.9",
+        distance: "A 110 km",
+        recommended: true
+      }
+    
+  ];
 }
