e29ce1dd0081ae36040bc954455bd2bfbee0edb2
Miguel Ángel <migue.rodrii08@gmail.com>
2025-02-20 18:50:28 +0000

    refactor categories section for dynamic rendering and improved styling

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -90,4 +90,8 @@ export class HomeComponent {
       }
     
   ];
+
+  categories: string[] = ['Playas', 'Montañas', 'Ciudades', 'Bosques', 'Desiertos', 'Lagos', 'Ríos', 'Cascadas', 'Pueblos', 'Parques', 'Monumentos', 'Museos', 'Iglesias', 'Castillos', 'Mercados', 'Fiestas', 'Eventos', 'Deportes', 'Aventura', 'Gastronomía', 'Vinos', 'Cervezas', 'Cafés', 'Restaurantes', 'Bares', 'Discotecas', 'Tiendas', 'Mercadillos', 'Artesanía', 'Moda', 'Joyas', 'Música', 'Teatro', 'Cine', 'Festivales', 'Conciertos', 'Exposiciones', 'Ferias', 'Carnavales', 'Fiestas', 'Eventos', 'Deportes', 'Aventura', 'Gastronomía', 'Vinos', 'Cervezas', 'Cafés', 'Restaurantes', 'Bares', 'Discotecas', 'Tiendas', 'Mercadillos', 'Artesanía', 'Moda', 'Joyas', 'Música', 'Teatro', 'Cine'];
+  categoryImage: string = 'https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg';
 }
+  
