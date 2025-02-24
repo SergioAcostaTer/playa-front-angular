7c6bf7f12b47d2030c6ea689302f9c24b641e441
Miguel Ángel <migue.rodrii08@gmail.com>
2025-02-24 16:47:10 +0000

    add profile component; implement profile page with form and styles; update routing to include profile path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,23 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginPageComponent } from './pages/login/login.component';
 import { NoHeaderLayoutComponent } from './layout/noheader-layout/noheader-layout.component';
 import { RegisterPageComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    children: [{ path: '', component: HomeComponent }],
+    children: [
+      { path: '', component: HomeComponent },
+      {path: 'profile', component: ProfileComponent}
+    ],
   },
   {
     path: '',
     component: NoHeaderLayoutComponent,
     children: [
       { path: 'register', component: RegisterPageComponent },
-      { path: 'login', component: LoginPageComponent },
+      { path: 'login', component: LoginPageComponent }
     ],
   },
   { path: '**', redirectTo: '/' },
