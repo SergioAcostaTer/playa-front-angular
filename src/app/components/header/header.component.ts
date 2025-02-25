8e4dbcd8f6353d4e16be47f7c7f0f4627034aac9
sergioacostaquintana <sergioacostaquintana@gmail.com>
2025-02-25 19:25:51 +0000

    fix header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: '[app-header]',
+  selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
-  encapsulation: ViewEncapsulation.ShadowDom,
-  host: { 'id': 'header' }
 })
 export class HeaderComponent {}
