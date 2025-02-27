bf2522e8a0d874654eda9d78385d57af1e36a468
Sergio Acosta Quintana <sergioacostaquintana@gmail.com>
2025-02-27 17:37:00 +0000

    Refactor ranking page layout and styles for improved design and responsiveness

--- src/app/pages/ranking/ranking.component.ts
+++ src/app/pages/ranking/ranking.component.ts
@@ -1,78 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BeachCardComponent } from '../../components/beach-card/beach-card.component';
-import { beachesList } from '../../constants/beachesList.js';
-
 @Component({
   selector: 'ranking-page',
   standalone: true,
-  imports: [CommonModule, BeachCardComponent],
+  imports: [CommonModule],
   templateUrl: './ranking.component.html',
   styleUrls: ['./ranking.component.css'],
 })
 export class RankingPageComponent {
-  beaches = beachesList;
-
-  categories: string[] = [
-    'Playas',
-    'Montañas',
-    'Ciudades',
-    'Bosques',
-    'Desiertos',
-    'Lagos',
-    'Ríos',
-    'Cascadas',
-    'Pueblos',
-    'Parques',
-    'Monumentos',
-    'Museos',
-    'Iglesias',
-    'Castillos',
-    'Mercados',
-    'Fiestas',
-    'Eventos',
-    'Deportes',
-    'Aventura',
-    'Gastronomía',
-    'Vinos',
-    'Cervezas',
-    'Cafés',
-    'Restaurantes',
-    'Bares',
-    'Discotecas',
-    'Tiendas',
-    'Mercadillos',
-    'Artesanía',
-    'Moda',
-    'Joyas',
-    'Música',
-    'Teatro',
-    'Cine',
-    'Festivales',
-    'Conciertos',
-    'Exposiciones',
-    'Ferias',
-    'Carnavales',
-    'Fiestas',
-    'Eventos',
-    'Deportes',
-    'Aventura',
-    'Gastronomía',
-    'Vinos',
-    'Cervezas',
-    'Cafés',
-    'Restaurantes',
-    'Bares',
-    'Discotecas',
-    'Tiendas',
-    'Mercadillos',
-    'Artesanía',
-    'Moda',
-    'Joyas',
-    'Música',
-    'Teatro',
-    'Cine',
-  ];
-  categoryImage: string =
-    'https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg';
 }
