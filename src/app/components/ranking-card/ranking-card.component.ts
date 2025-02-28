9086ca14c24ef473f53d6d0e74f3a55e7cd2f685
Miguel Ángel <migue.rodrii08@gmail.com>
2025-02-28 18:37:39 +0000

    Add ranking components: create RankingList and RankingCard components with styles and integrate into the ranking page

---
+++ src/app/components/ranking-card/ranking-card.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ranking-card',
+  templateUrl: './ranking-card.component.html',
+  styleUrls: ['./ranking-card.component.css'],
+  imports: [ CommonModule ]
+})
+export class RankingCardComponent {
+  @Input() title!: string;
+  @Input() imageUrl!: string;
+  @Input() rating!: number;
+  @Input() distance!: string;
+  @Input() description!: string;
+  @Input() recommended: boolean = false;
+}
--- src/app/pages/ranking/ranking.component.ts
+++ src/app/pages/ranking/ranking.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TitlePageComponent } from "../../components/title-page/title-page.component";
+import { RankingListComponent } from '../../components/ranking-list/ranking-list.component';
 @Component({
   selector: 'ranking-page',
   standalone: true,
-  imports: [CommonModule, TitlePageComponent],
+  imports: [CommonModule, TitlePageComponent, RankingListComponent],
   templateUrl: './ranking.component.html',
   styleUrls: ['./ranking.component.css'],
 })
