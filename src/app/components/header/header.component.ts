ed4f654230c774512ab5936175a0eb43b1a5be06
Miguel Ángel <migue.rodrii08@gmail.com>
2025-03-11 19:59:25 +0000

    Add user popup component to header with toggle functionality

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,31 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  imports: [CommonModule],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isPopupVisible: boolean = false;
+
+  togglePopup(): void {
+    this.isPopupVisible = !this.isPopupVisible;
+  }
+
+  closePopup(): void {
+    this.isPopupVisible = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event): void {
+    const target = event.target as HTMLElement;
+
+    // Verifica si el clic fue en el avatar o dentro del popup
+    if (!target.closest('.user-popup-container') && !target.closest('#avatar')) {
+      this.closePopup();
+    }
+  }
+}
