61a0ece90235d505d9b19e1a8a4426a320f979d2
Miguel Ángel <migue.rodrii08@gmail.com>
2025-03-12 18:36:54 +0000

    refactor: Remove unused CSS file and update favourite component to improve maintainability

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
+  standalone: true,
   imports: [ RouterModule ],
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css'],
