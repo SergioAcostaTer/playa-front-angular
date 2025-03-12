3933e0d54e4e1b79572000a2f1e1fc9660e61d31
Miguel Ángel <migue.rodrii08@gmail.com>
2025-03-12 17:23:07 +0000

    refactor: Update footer component selector to follow Angular conventions

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'Footer',
+  selector: 'app-footer',
   standalone: true,
   templateUrl: './footer.component.html',
   styleUrls: [ './footer.component.css' ],
