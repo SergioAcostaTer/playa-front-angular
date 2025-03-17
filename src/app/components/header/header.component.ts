592ceaafc5956dacf0da2098c3c7a90d37d09a60
Miguel Ángel <migue.rodrii08@gmail.com>
2025-03-17 20:28:49 +0000

    style: Refactor header component and add logo component for improved structure and clarity

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrls: [ './header.component.css' ],
-  imports: [CommonModule],
+  imports: [ CommonModule, LogoComponent ],
 })
 export class HeaderComponent {
   isPopupVisible: boolean = false;
@@ -19,12 +20,14 @@ export class HeaderComponent {
     this.isPopupVisible = false;
   }
 
+  // user: any = fetch('/api/user');
+
   @HostListener('document:click', ['$event'])
   onClickOutside(event: Event): void {
     if (!this.isPopupVisible) return;
 
     const target = event.target as HTMLElement;
-    if (!target.closest('.header__popup') && !target.closest('.header__menu')) {
+    if (!target.closest('.header__popup') && !target.closest('.header__menu-toggle')) {
       this.closePopup();
     }
   }
