d96e0245b0df7511f3b9cf5fb6405f1e4bba4b95
sergio <sergio>
2025-03-25 20:26:47 +0000

    feat: enhance home page banner with improved layout and search functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,25 @@
-// src/app/home.component.ts
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { CategoryListComponent } from '../../components/category-list/category-list.component';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+// import { CategoryListComponent } from '../../components/category-list/category-list.component';
 import { BeachGridComponent } from '../../components/beach-grid/beach-grid.component';
-import { getBeaches } from '../../services/getBeaches';
-import { getCategories } from '../../services/getCategories';
 import { Beach } from '../../models/beach';
 import { Category } from '../../models/category';
+import { getCategories } from '../../services/getCategories';
+import { getBeaches } from '../../services/getBeaches';
+import { CategoryListComponent } from "../../components/category-list/category-list.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CategoryListComponent, BeachGridComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    // CategoryListComponent,
+    BeachGridComponent,
+    CategoryListComponent
+],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
@@ -19,15 +27,23 @@ export class HomePageComponent implements OnInit {
   categories: Category[] = [];
   beaches: Beach[] = [];
   loading = true;
+  searchQuery: string = '';
+
+  constructor(private router: Router) {}
 
   async ngOnInit() {
     try {
       this.beaches = await getBeaches();
       this.categories = await getCategories();
     } catch (error) {
-      console.error('Error fetching beaches:', error);
     } finally {
       this.loading = false;
+    }
+  }
+
+  searchBeaches() {
+    if (this.searchQuery.trim()) {
+      this.router.navigate(['/search'], { queryParams: { q: this.searchQuery.trim() } });
     }
   }
 }
